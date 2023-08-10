260a1dd00ab9b38af871dc6898e715ed5ecf2fff
Wyatt Barnes <wyattlbarnes@gmail.com>
2023-08-10 15:56:11 -0700

    Finished twelve website

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template("pages/home.html")
 
+@app.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@app.route("/careers")
+def careers():
+    return render_template("pages/careers.html")
+
+@app.route("/events")
+def events():
+    return render_template("pages/events.html")
+
 @app.route("/the-trial")
 def the_trial():
     return render_template("pages/the-trial.html")
